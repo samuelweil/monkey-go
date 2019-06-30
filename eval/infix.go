b583a09bc93b0110200a7c87a5ca86617407da6f
Sam Weil <s.weil9391@gmail.com>
2019-06-30 16:43:36 -0500

    Add boolean infix for ints

--- eval/infix.go
+++ eval/infix.go
@@ -20,20 +20,49 @@ func evalIntegerInfix(op string, l, r object.Object) object.Object {
 	leftVal := l.(*object.Integer).Value
 	rightVal := r.(*object.Integer).Value
 
-	switch op {
-	case "+":
-		return &object.Integer{Value: leftVal + rightVal}
+	if evaluator, ok := integerInfixes[op]; ok {
+		return doEval(evaluator, leftVal, rightVal)
+	}
 
-	case "-":
-		return &object.Integer{Value: leftVal - rightVal}
+	return Null
+}
 
-	case "*":
-		return &object.Integer{Value: leftVal * rightVal}
+func doEval(e intInfixEvaluator, l, r int) object.Object {
 
-	case "/":
-		return &object.Integer{Value: leftVal / rightVal}
+	result := e(l, r)
+	
+	switch v := result.(type) {
+	
+	case bool:
+		return boolean(v)
+	
+	case int:
+		return &object.Integer{Value: v}
 
 	default:
 		return Null
 	}
 }
+
+type intInfixEvaluator func(l, r int) interface{}
+
+var integerInfixes = map[string]intInfixEvaluator {
+	"+": add,
+	"-": subtract,
+	"*": multiply,
+	"/": divide,
+	"==": equal,
+	"!=": notEqual,
+	"<": less,
+	">": greater,
+}
+
+func add(l, r int) interface{} { return l + r }
+func subtract(l, r int) interface{} { return l - r}
+func multiply(l, r int) interface{} {return l * r }
+func divide(l, r int) interface{} { return l / r}
+
+func equal(l, r int) interface{} { return l == r }
+func notEqual(l, r int) interface{} { return l != r}
+func less(l, r int) interface{} { return l < r}
+func greater(l, r int) interface{} { return l > r}
